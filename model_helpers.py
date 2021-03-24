fcdd13cfba3100cb2973cbb7ffab2197c161fac9
Derek Powell <dmpowell@derek-imac.local>
2021-03-24 16:23:32 -0700

    fixed PT+N, implemented hierarchial models

--- model_helpers.py
+++ model_helpers.py
@@ -2,20 +2,14 @@ import jax.numpy as jnp
 import numpy as np
 
 
-def is_conjdisj(trial):
-    conjdisj = set(["AorB","notAorB","AornotB","notAornotB", "AandB", "notAandB", "AandnotB", "notAandnotB"])
-    x = 1 if trial in conjdisj else 0
-    return x
+### ------ Data processing
 
-def load_data():
+def load_query_avg_data():
     import glob
     df = pd.concat([pd.read_csv(f) for f in glob.glob('osfstorage-archive/Experiment 2/*.csv')], ignore_index = True)
 
-    fix_id_dict = dict()
     original_ids = list(np.unique(df.ID))
-
-    for i in range(0, len(original_ids)):
-        fix_id_dict[original_ids[i]] = i
+    fix_id_dict = {original_ids[i]:i for i in range(0, len(original_ids))}
 
     df = (df >> 
          filter_by(~X.querydetail.str.contains("warm|snowy")) >>
@@ -24,34 +18,40 @@ def load_data():
           summarize(estimate = np.mean(X.estimate)) >>
           mutate(coldrainy = X.querydetail.str.contains("cold|rainy").astype("int")) >>
           mutate(estimate = X.estimate.replace({0:.01, 1:.99})) >>
-          mutate(conjdisj_trial = X.querytype.apply(is_conjdisj)) >>
-          mutate(ID = X.ID.apply(lambda x: fix_id_dict[x]))
+#           mutate(conjdisj_trial = X.querytype.apply(is_conjdisj)) >>
+          mutate(ID = X.ID.apply(lambda x: fix_id_dict[x])) >>
+          ungroup()
          )
     
     return df
 
-def make_model_data(data):
-    df_test = data
-    # df_test = df >> filter_by(X.conjdisj_trial==0)
-    # df_test = df >> filter_by(X.coldrainy==1)
 
-    trial_list = df_test.querytype
-    X_num = jnp.stack([num_vecs[trial] for trial in trial_list])
-    X_denom = jnp.stack([denom_vecs[trial] for trial in trial_list])
+def make_model_data(data):
 
     X_data = {
-        "trial": trial_list,
-        "X_num": X_num,
-        "X_denom": X_denom,
-        "subj": jnp.array(list(df_test.ID)),
-        "conjdisj_trial": jnp.array(list(df_test.conjdisj_trial)),
-        "cond": jnp.array(list(df_test.coldrainy)).astype("int32")
-    #     "block": jnp.array(list(df_test.block))
+        "trial": data.querytype,
+        "subj": jnp.array(list(data.ID)),
+        "cond": jnp.array(list(data.coldrainy), dtype="int32"),
     }
 
-    y_data = jnp.array(df_test.estimate.to_numpy())
+    y_data = jnp.array(data.estimate.to_numpy())
     
     return X_data, y_data
+
+
+### -------- Simulation functions
+
+def sim_sampling(p, beta, N, k):
+
+    p_bs = p * N / (N + 2.*beta) + beta/(N + 2.*beta)
+#     return np.random.normal(p_bs, k)
+    return np.random.beta(p_bs*k, (1-p_bs)*k)
+
+def calc_prob(trial, theta):
+    ## compute implied subj. probability from latent theta and trial type
+    ## this is a vectorized solution: https://bit.ly/2P6mMcD
+    
+    return trial_funcs[trial](theta)
 
 trial_funcs = dict({
     "AandB": lambda theta: jnp.matmul(theta, jnp.array([1.,0.,0.,0.])) ,
@@ -77,17 +77,39 @@ trial_funcs = dict({
     "notBgnotA": lambda theta: jnp.divide( jnp.matmul(theta, jnp.array([0.,0.,0.,1.])), jnp.matmul(theta, jnp.array([0.,0.,1.,1.])) )
 })
 
-def sim_sampling(p, beta, N, k):
 
-    p_bs = p * N / (N + 2.*beta) + beta/(N + 2.*beta)
-#     return np.random.normal(p_bs, k)
-    return np.random.beta(p_bs*k, (1-p_bs)*k)
 
-def calc_prob(trial, theta):
-    ## compute implied subj. probability from latent theta and trial type
-    ## this is a vectorized solution: https://bit.ly/2P6mMcD
+def sim_bayesian_sampler(trial_types, n_participants, n_blocks, params):
+
+    n_trial_types = len(trial_types)
+    trials = trial_types*n_blocks
+    conds = np.tile(np.array([0,1]).repeat(len(trial_types)), n_participants)
+    blocks = list(np.repeat(np.array(list(range(0, n_blocks))), n_trial_types))
+
+    all_participants = list(np.repeat(np.array(list(range(0, n_participants))), n_trial_types*n_blocks))
+    all_thetas = make_thetas(n_participants)
+    all_trials = trials*n_participants
+    all_blocks = blocks*n_participants
+
+    sim_data = pd.DataFrame(
+        data = {
+            "ID": all_participants,
+            "querytype": all_trials, 
+            "block": all_blocks,
+            "coldrainy": conds
+        }) >> mutate(
+            conjdisj_trial = X.querytype.apply(is_conjdisj),
+            N_base = X.ID.apply(lambda x: params["N_base"][x]),
+            N_delta = X.ID.apply(lambda x: params["N_delta"][x]),
+            beta = X.ID.apply(lambda x: params["beta"][x]),
+        theta = X.ID.apply(lambda x: all_thetas[x])
+        ) >> mutate(N = X.N_base + X.N_delta * abs(1-X.conjdisj_trial))
+
+    sim_data["prob"] = sim_data.apply(lambda x: calc_prob(x.querytype, x.theta), axis=1)
+    sim_data["estimate"] = sim_data.apply(lambda x: sim_sampling(x.prob, x.beta, x.N, params["k"]), axis=1)
     
-    return trial_funcs[trial](theta)
+    return sim_data
+
 
 # def sim_sampling(p, beta, N, k):
 
@@ -169,6 +191,21 @@ def make_thetas(n):
         
 #     return all_responses
 
+#### ---- model construction / design matrix construction
+
+def sigmoid(x):  
+    return jnp.exp(-jnp.logaddexp(0, -x)) # numerically stable
+
+
+def is_cond(trial):
+    return float(bool(re.search("g",trial)))
+
+
+def is_conjdisj(trial):
+    conjdisj = set(["AorB","notAorB","AornotB","notAornotB", "AandB", "notAandB", "AandnotB", "notAandnotB"])
+    x = 1 if trial in conjdisj else 0
+    return x
+
 
 num_vecs = dict({
     "AandB": jnp.array([1.,0.,0.,0.]),
@@ -217,6 +254,34 @@ denom_vecs = dict({
     "BgnotA": jnp.array([0.,0.,1.,1.]),
     "notBgnotA": jnp.array([0.,0.,1.,1.])
 })
+
+
+pA_vecs = dict({
+    "AandB": jnp.ones(4), # unused
+    "AandnotB": jnp.ones(4),
+    "notAandB": jnp.ones(4),
+    "notAandnotB": jnp.ones(4),
+    "A":jnp.ones(4),
+    "B":jnp.ones(4),
+    "notA":jnp.ones(4),
+    "notB":jnp.ones(4),
+    "AorB":jnp.ones(4),
+    "AornotB":jnp.ones(4),
+    "notAorB":jnp.ones(4),
+    "notAornotB":jnp.ones(4),
+   
+    "AgB": jnp.array([1.,1.,0.,0.]),
+    "notAgB": jnp.array([0.,0.,1.,1.]),
+    "AgnotB": jnp.array([1.,1.,0.,0.]),
+    "notAgnotB": jnp.array([0.,0.,1.,1.]),
+    "BgA": jnp.array([1.,0.,1.,0.]),
+    "notBgA": jnp.array([0.,1.,0.,1.]),
+    "BgnotA": jnp.array([1.,0.,1.,0.]),
+    "notBgnotA": jnp.array([0.,1.,0.,1.])
+})
+
+
+### ------ Arviz
 
 def make_arviz_data(mcmc, model, data):
     posterior_samples = mcmc.get_samples()
